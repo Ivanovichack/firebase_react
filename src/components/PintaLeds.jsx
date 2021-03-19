121de7df204a0b4bc297ececf72e22081a066e39
Ivan Luis Jimenez <ILuis>
2021-03-19 13:58:30 -0600

    Agregando cambios menores

--- src/components/PintaLeds.jsx
+++ src/components/PintaLeds.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
-class PintaLeds extends Component {
+class PintaLeds extends Component {    
+    
+    constructor(){
+        let a=0,b=0,c=0;
+    }
+
     state = {
         r: this.props.items[0],
         g: this.props.items[1],
         b: this.props.items[2]
     }
+
+    
     render(){
         return(
             <div className="row">
@@ -14,18 +21,17 @@ class PintaLeds extends Component {
                     <input type="button" className="btn btn-success" value={this.state.g}/>
                     <input type="button" className="btn btn-info" value={this.state.b}/>
                 </div>
-                <div className="col-md-8">
+                <div className="col-md-6">
                     <div className="row">                        
-                        <input type="range" min="0" max="255" class="form-range" />
+                        <input type="range" min="0" max="255" step="1" class="form-range" value={this.a}/>
                     </div>
                     <div className="row">
-                        <input type="range" min="0" max="255" class="form-range" />
+                        <input type="range" min="0" max="255" step="1" class="form-range" />
                     </div>
                     <div className="row">
-                        <input type="range" min="0" max="255" class="form-range" />
+                        <input type="range" min="0" max="255" step="1" class="form-range" />
                     </div>
-                </div>
-                
+                </div>                
             </div>
         );
     }
